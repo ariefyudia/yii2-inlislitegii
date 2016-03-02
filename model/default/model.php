36b76f47c27902532d5c9c534d8c3dc0492e6551
inlislite <alvina140404@gmail.com>
2016-03-02 17:02:50 +0700

    Update model.php
    add AR History

--- model/default/model.php
+++ model/default/model.php
@@ -110,6 +110,7 @@ if(!empty($enum)){
     public function behaviors()
     {
         return [
+        \nhkey\arh\ActiveRecordHistoryBehavior::className(),
 <?php if($generator->createdAt || $generator->updatedAt):?>
             [
                 'class' => TimestampBehavior::className(),
